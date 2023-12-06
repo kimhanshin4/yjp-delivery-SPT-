11468cc7fc89b94fc1cb54d8eeb5d102a5fa5b22
asdvsvs <gag973000@gmail.com>
2023-12-06 18:30:28 +0900

    Feat : AddShopRes 추가

---
+++ src/main/java/com/yjp/delivery/controller/admin/shop/dto/response/AddShopRes.java
@@ -0,0 +1,18 @@
+package com.yjp.delivery.controller.admin.shop.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddShopRes {
+
+  private String shopName;
+  private String description;
+  private String location;
+  private String callNumber;
+}
