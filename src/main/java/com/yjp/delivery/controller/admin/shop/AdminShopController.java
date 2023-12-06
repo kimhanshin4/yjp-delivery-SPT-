38ac6461b2cfcebcc520ab50d4157b17c3071adb
asdvsvs <gag973000@gmail.com>
2023-12-06 18:40:58 +0900

    Feat : AdminShopController updateShop() 추가

--- src/main/java/com/yjp/delivery/controller/admin/shop/AdminShopController.java
+++ src/main/java/com/yjp/delivery/controller/admin/shop/AdminShopController.java
@@ -2,9 +2,12 @@ package com.yjp.delivery.controller.admin.shop;
 
 import com.yjp.delivery.common.response.RestResponse;
 import com.yjp.delivery.controller.admin.shop.dto.request.AddShopReq;
+import com.yjp.delivery.controller.admin.shop.dto.request.UpdateShopReq;
 import com.yjp.delivery.controller.admin.shop.dto.response.AddShopRes;
+import com.yjp.delivery.controller.admin.shop.dto.response.UpdateShopRes;
 import com.yjp.delivery.service.AdminShopService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +23,10 @@ public class AdminShopController {
   @PostMapping
   public RestResponse<AddShopRes> addShop(@RequestBody AddShopReq addShopReq) {
     return RestResponse.success(adminShopService.addShop(addShopReq));
+  }
+
+  @PatchMapping
+  public RestResponse<UpdateShopRes> updateShop(@RequestBody UpdateShopReq updateShopReq) {
+    return RestResponse.success(adminShopService.updateShop(updateShopReq));
   }
 }
