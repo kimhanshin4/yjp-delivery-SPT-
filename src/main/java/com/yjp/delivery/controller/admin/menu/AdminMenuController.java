b8f4235d1a45867b1945f6b1a9c1eb6f055ef9b3
asdvsvs <gag973000@gmail.com>
2023-12-07 00:43:50 +0900

    Feat : AdminMenuController deleteMenu() 추가

--- src/main/java/com/yjp/delivery/controller/admin/menu/AdminMenuController.java
+++ src/main/java/com/yjp/delivery/controller/admin/menu/AdminMenuController.java
@@ -2,11 +2,14 @@ package com.yjp.delivery.controller.admin.menu;
 
 import com.yjp.delivery.common.response.RestResponse;
 import com.yjp.delivery.controller.admin.menu.dto.request.AddMenuReq;
+import com.yjp.delivery.controller.admin.menu.dto.request.DeleteMenuReq;
 import com.yjp.delivery.controller.admin.menu.dto.request.UpdateMenuReq;
 import com.yjp.delivery.controller.admin.menu.dto.response.AddMenuRes;
+import com.yjp.delivery.controller.admin.menu.dto.response.DeleteMenuRes;
 import com.yjp.delivery.controller.admin.menu.dto.response.UpdateMenuRes;
 import com.yjp.delivery.service.AdminMenuService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,5 +31,10 @@ public class AdminMenuController {
     @PatchMapping
     public RestResponse<UpdateMenuRes> updateMenu(@RequestBody UpdateMenuReq updateMenuReq) {
         return RestResponse.success(adminMenuService.updateMenu(updateMenuReq));
+    }
+
+    @DeleteMapping
+    public RestResponse<DeleteMenuRes> deleteMenu(@RequestBody DeleteMenuReq deleteMenuReq) {
+        return RestResponse.success(adminMenuService.deleteMenu(deleteMenuReq));
     }
 }
