b4c3bd831d7f5c572f5efca1b03c6ef803a3d52d
asdvsvs <gag973000@gmail.com>
2023-12-07 00:36:20 +0900

    Feat : MenuValidator checkIsNull() 추가

--- src/main/java/com/yjp/delivery/common/meta/ResultCode.java
+++ src/main/java/com/yjp/delivery/common/meta/ResultCode.java
@@ -5,7 +5,8 @@ public enum ResultCode {
     SYSTEM_ERROR(1000, "알 수 없는 애러가 발생했습니다."),
     NOT_FOUND_SAMPLE(1001, "샘플 데이터가 없습니다."),
     NOT_FOUND_USER(2000, "존재하지 않는 유저입니다."),
-    NOT_FOUND_SHOP(4001, "가게 데이터가 없습니다.");
+    NOT_FOUND_SHOP(4001, "가게 데이터가 없습니다."),
+    NOT_FOUND_MENU(5001, "메뉴 데이터가 없습니다.");
 
     private Integer code;
     private String message;
