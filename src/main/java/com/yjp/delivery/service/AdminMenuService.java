a93411a3f47da2ab8ab2421753ed74468ab544a0
asdvsvs <gag973000@gmail.com>
2023-12-07 00:35:26 +0900

    Feat : AdminMenuService updateMenu() 추가

--- src/main/java/com/yjp/delivery/service/AdminMenuService.java
+++ src/main/java/com/yjp/delivery/service/AdminMenuService.java
@@ -1,8 +1,11 @@
 package com.yjp.delivery.service;
 
+import com.yjp.delivery.common.validator.MenuValidator;
 import com.yjp.delivery.common.validator.ShopValidator;
 import com.yjp.delivery.controller.admin.menu.dto.request.AddMenuReq;
+import com.yjp.delivery.controller.admin.menu.dto.request.UpdateMenuReq;
 import com.yjp.delivery.controller.admin.menu.dto.response.AddMenuRes;
+import com.yjp.delivery.controller.admin.menu.dto.response.UpdateMenuRes;
 import com.yjp.delivery.store.entity.MenuEntity;
 import com.yjp.delivery.store.entity.ShopEntity;
 import com.yjp.delivery.store.repository.MenuRepository;
@@ -31,11 +34,28 @@ public class AdminMenuService {
                 .build()));
     }
 
+    public UpdateMenuRes updateMenu(UpdateMenuReq updateMenuReq) {
+        ShopEntity shopEntity = shopRepository.findByShopId(updateMenuReq.getShopId());
+        ShopValidator.validate(shopEntity);
+        MenuEntity menuEntity = menuRepository.findByMenuId(updateMenuReq.getMenuId());
+        MenuValidator.validate(menuEntity);
+        return AdminMenuServiceMapper.INSTANCE.toUpdateMenuRes(
+            menuRepository.save(MenuEntity.builder()
+                .menuId(updateMenuReq.getMenuId())
+                .imageUrl(updateMenuReq.getImageUrl())
+                .menuName(updateMenuReq.getMenuName())
+                .price(updateMenuReq.getPrice())
+                .shopEntity(shopEntity)
+                .build()));
+    }
+
     @Mapper
     public interface AdminMenuServiceMapper {
 
         AdminMenuServiceMapper INSTANCE = Mappers.getMapper(AdminMenuServiceMapper.class);
 
         AddMenuRes toAddMenuRes(MenuEntity menuEntity);
+
+        UpdateMenuRes toUpdateMenuRes(MenuEntity menuEntity);
     }
 }
