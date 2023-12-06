60f7c31d1e2f0502783dd9bfa91755337810e0cd
asdvsvs <gag973000@gmail.com>
2023-12-06 18:41:23 +0900

    Feat : AdminShopService updateShop() 추가

--- src/main/java/com/yjp/delivery/service/AdminShopService.java
+++ src/main/java/com/yjp/delivery/service/AdminShopService.java
@@ -1,7 +1,10 @@
 package com.yjp.delivery.service;
 
+import com.yjp.delivery.common.validator.ShopValidator;
 import com.yjp.delivery.controller.admin.shop.dto.request.AddShopReq;
+import com.yjp.delivery.controller.admin.shop.dto.request.UpdateShopReq;
 import com.yjp.delivery.controller.admin.shop.dto.response.AddShopRes;
+import com.yjp.delivery.controller.admin.shop.dto.response.UpdateShopRes;
 import com.yjp.delivery.store.entity.ShopEntity;
 import com.yjp.delivery.store.repository.ShopRepository;
 import lombok.RequiredArgsConstructor;
@@ -25,11 +28,25 @@ public class AdminShopService {
             .build()));
   }
 
+  public UpdateShopRes updateShop(UpdateShopReq updateShopReq) {
+    ShopEntity shopEntity = shopRepository.findByShopName(updateShopReq.getShopName());
+    ShopValidator.validate(shopEntity);
+    shopEntity = ShopEntity.builder()
+        .shopName(updateShopReq.getNewShopName())
+        .description(updateShopReq.getDescription())
+        .location(updateShopReq.getLocation())
+        .callNumber(updateShopReq.getCallNumber())
+        .build();
+    return AdminShopServiceMapper.INSTANCE.toUpdateShopRes(shopEntity);
+  }
+
   @Mapper
   public interface AdminShopServiceMapper {
 
     AdminShopServiceMapper INSTANCE = Mappers.getMapper(AdminShopServiceMapper.class);
 
     AddShopRes toAddShopRes(ShopEntity shopEntity);
+
+    UpdateShopRes toUpdateShopRes(ShopEntity shopEntity);
   }
 }
