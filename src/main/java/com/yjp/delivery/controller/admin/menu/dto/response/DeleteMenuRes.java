37add17fc75f311bf5a4f8865ff37ed3897e3eb6
asdvsvs <gag973000@gmail.com>
2023-12-07 17:09:37 +0900

    Refactor : DeleteMenuRes 쓸모 없는 애노테이션 제거

--- src/main/java/com/yjp/delivery/controller/admin/menu/dto/response/DeleteMenuRes.java
+++ src/main/java/com/yjp/delivery/controller/admin/menu/dto/response/DeleteMenuRes.java
@@ -1,11 +1,7 @@
 package com.yjp.delivery.controller.admin.menu.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-@Builder
 @JsonIgnoreProperties
 public class DeleteMenuRes {
 
