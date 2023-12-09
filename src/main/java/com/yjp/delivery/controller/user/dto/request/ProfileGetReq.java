b889c6fc007049a64db4ab02245bb5caa95a3c69
emost22 <emoney96@naver.com>
2023-12-09 22:57:32 +0900

    Test: Add get user

--- src/main/java/com/yjp/delivery/controller/user/dto/request/ProfileGetReq.java
+++ src/main/java/com/yjp/delivery/controller/user/dto/request/ProfileGetReq.java
@@ -1,8 +1,10 @@
 package com.yjp.delivery.controller.user.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ProfileGetReq {
 
     private Long userId;
