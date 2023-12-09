554bd252a5f5b99e8c823d9e8740247b0a7feae6
emost22 <emoney96@naver.com>
2023-12-09 22:22:38 +0900

    Test: Add get orders by userId

--- src/test/java/com/yjp/delivery/service/OrderServiceTest.java
+++ src/test/java/com/yjp/delivery/service/OrderServiceTest.java
@@ -7,9 +7,11 @@ import static org.mockito.Mockito.when;
 
 import com.yjp.delivery.common.meta.OrderStatus;
 import com.yjp.delivery.controller.order.dto.request.OrderGetShopReq;
+import com.yjp.delivery.controller.order.dto.request.OrderGetUserReq;
 import com.yjp.delivery.controller.order.dto.request.OrderSaveReq;
 import com.yjp.delivery.controller.order.dto.request.OrderSaveReqList;
 import com.yjp.delivery.controller.order.dto.response.OrderGetShopRes;
+import com.yjp.delivery.controller.order.dto.response.OrderGetUserRes;
 import com.yjp.delivery.controller.order.dto.response.OrderSaveRes;
 import com.yjp.delivery.controller.order.dto.response.OrderSaveResList;
 import com.yjp.delivery.service.provider.OrderDetailProvider;
@@ -121,5 +123,44 @@ class OrderServiceTest {
         assertThat(orderGetShopRes.getTotal()).isEqualTo(1);
         assertThat(orderGetShopRes.getOrderGetResWrappers().get(0).getOrderId()).isEqualTo(orderId);
         verify(orderRepository).findByShopId(any());
+    }
+
+    @Test
+    @DisplayName("userId로 주문 정보 조회 테스트")
+    void userId_주문_정보_조회() {
+        // given
+        Long shopId = 1L;
+        Long orderId = 1L;
+        Long menuId = 1L;
+        Long userId = 1L;
+        OrderGetUserReq orderGetUserReq = OrderGetUserReq.builder()
+            .userId(userId)
+            .build();
+        ShopEntity shop = ShopEntity.builder().shopId(shopId).build();
+        MenuEntity menu = MenuEntity.builder().menuId(menuId).shopEntity(shop).build();
+        UserEntity user = UserEntity.builder().userId(userId).build();
+        OrderEntity order = OrderEntity.builder()
+            .orderId(orderId)
+            .userEntity(user)
+            .build();
+        OrderDetailEntity orderDetailEntity = OrderDetailEntity.builder()
+            .orderEntity(order)
+            .menuEntity(menu)
+            .build();
+        order = OrderEntity.builder()
+            .orderId(orderId)
+            .userEntity(user)
+            .orderDetailEntities(List.of(orderDetailEntity))
+            .build();
+        List<OrderEntity> orders = List.of(order);
+        when(orderRepository.findByUserEntityUserId(any())).thenReturn(orders);
+
+        // when
+        OrderGetUserRes orderGetUserRes = orderService.getAllOrderByUser(orderGetUserReq);
+
+        // then
+        assertThat(orderGetUserRes.getTotal()).isEqualTo(1);
+        assertThat(orderGetUserRes.getOrderGetResWrappers().get(0).getOrderId()).isEqualTo(orderId);
+        verify(orderRepository).findByUserEntityUserId(any());
     }
 }
