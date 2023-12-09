a97636c6295bf82c1a202a1cfb57f79fe6676179
emost22 <emoney96@naver.com>
2023-12-10 05:02:05 +0900

    Test: Add shop like api

--- src/test/java/com/yjp/delivery/controller/shop/ShopControllerTest.java
+++ src/test/java/com/yjp/delivery/controller/shop/ShopControllerTest.java
@@ -3,12 +3,15 @@ package com.yjp.delivery.controller.shop;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.yjp.delivery.controller.BaseMvcTest;
+import com.yjp.delivery.controller.shop.dto.request.ShopLikeReq;
 import com.yjp.delivery.controller.shop.dto.response.ShopGetAllRes;
 import com.yjp.delivery.controller.shop.dto.response.ShopGetRes;
+import com.yjp.delivery.controller.shop.dto.response.ShopLikeRes;
 import com.yjp.delivery.service.ShopLikeService;
 import com.yjp.delivery.service.ShopService;
 import java.util.List;
@@ -16,6 +19,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 
 @WebMvcTest(controllers = {ShopController.class})
 class ShopControllerTest extends BaseMvcTest {
@@ -52,6 +56,25 @@ class ShopControllerTest extends BaseMvcTest {
         this.mockMvc
             .perform(
                 get("/v1/shop/" + shopId))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("가게 좋아요 테스트")
+    void 가게_좋아요_테스트() throws Exception {
+        Long userId = 1L;
+        Long shopId = 1L;
+        Boolean isLike = true;
+        ShopLikeReq shopLikeReq = ShopLikeReq.builder()
+            .userId(userId).shopId(shopId).isLike(isLike).build();
+        ShopLikeRes result = new ShopLikeRes();
+        when(shopLikeService.likeShop(any())).thenReturn(result);
+        this.mockMvc
+            .perform(
+                post("/v1/shop/like")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(shopLikeReq)))
             .andDo(print())
             .andExpect(status().isOk());
     }
