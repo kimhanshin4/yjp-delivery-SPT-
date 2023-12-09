b5420395158e9be02cf2d9a3b2d03bfe59fcc348
emost22 <emoney96@naver.com>
2023-12-09 23:01:22 +0900

    Test: Add update user

--- src/test/java/com/yjp/delivery/service/UserServiceTest.java
+++ src/test/java/com/yjp/delivery/service/UserServiceTest.java
@@ -5,11 +5,14 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.yjp.delivery.controller.user.dto.request.ProfileEditReq;
 import com.yjp.delivery.controller.user.dto.request.ProfileGetReq;
+import com.yjp.delivery.controller.user.dto.response.ProfileEditRes;
 import com.yjp.delivery.controller.user.dto.response.ProfileGetRes;
 import com.yjp.delivery.service.provider.S3Provider;
 import com.yjp.delivery.store.entity.UserEntity;
 import com.yjp.delivery.store.repository.UserRepository;
+import java.io.IOException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +20,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.multipart.MultipartFile;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
@@ -46,5 +50,37 @@ class UserServiceTest {
         // then
         assertThat(profileGetRes.getUsername()).isEqualTo(username);
         verify(userRepository).findByUserId(any());
+    }
+
+    @Test
+    @DisplayName("프로필 수정 테스트")
+    void 프로필_수정() throws IOException {
+        // given
+        Long userId = 1L;
+        String username = "ysys";
+        String updatedUsername = "ysysys";
+        MultipartFile multipartFile = null;
+        ProfileEditReq profileEditReq = ProfileEditReq.builder()
+            .userId(userId)
+            .username(updatedUsername)
+            .build();
+        UserEntity user = UserEntity.builder()
+            .userId(userId)
+            .username(username)
+            .build();
+        UserEntity updatedUser = UserEntity.builder()
+            .userId(userId)
+            .username(updatedUsername)
+            .build();
+        when(userRepository.findByUserId(any())).thenReturn(user);
+        when(userRepository.save(any())).thenReturn(updatedUser);
+
+        // when
+        ProfileEditRes profileEditRes = userService.editUserProfile(multipartFile, profileEditReq);
+
+        // then
+        assertThat(profileEditRes.getUsername()).isEqualTo(updatedUsername);
+        verify(userRepository).findByUserId(any());
+        verify(userRepository).save(any());
     }
 }
