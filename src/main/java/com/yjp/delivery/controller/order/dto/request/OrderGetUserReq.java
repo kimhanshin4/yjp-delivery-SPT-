698500380b2a52cd75d7a61c1e133a8a94347461
emost22 <emoney96@naver.com>
2023-12-09 21:37:07 +0900

    Feat: Add get order dtos

---
+++ src/main/java/com/yjp/delivery/controller/order/dto/request/OrderGetUserReq.java
@@ -0,0 +1,15 @@
+package com.yjp.delivery.controller.order.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderGetUserReq {
+
+    private Long userId;
+}
